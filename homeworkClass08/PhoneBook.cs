794d34e5590346c99c66be139f93f325744d734e
Nikola <nikoladalchevski@gmail.com>
2019-03-22 11:37:25 +0100

    homework

---
+++ homeworkClass08/PhoneBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace homeworkClass08
+{
+    public class PhoneBook
+    {
+        public Dictionary<string, int> phoneBook = new Dictionary<string, int>
+        {
+               {"Venko", 078346432  },
+                {"Nikola", 077325234 },
+                {"Sanja",  078345236 },
+                {"Vukasin", 0702354865 },
+                {"Marko", 034587634 }
+        };
+        
+
+     
+
+
+
+    }
+}
---
+++ homeworkClass08/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeworkClass08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var yourPhoneBook = new PhoneBook();
+            bool askAgain = true;
+            while (askAgain)
+            {
+                
+                Console.WriteLine($"Search for name in phonebook:");
+                string userInput = Console.ReadLine();
+                var empty = string.IsNullOrEmpty(userInput);
+                if (empty)
+                {
+                    Console.WriteLine("You must enter a name! Do you whant to continue Y/n");
+                    var userInput2 = Console.ReadLine();
+                    if(userInput2 == "n" || userInput2 == "N")
+                    {
+                        askAgain = false;
+                        break;
+                    }
+                    break;
+                }
+
+                var isfound = yourPhoneBook.phoneBook.ContainsKey(userInput);
+                if (isfound)
+                {
+                    Console.WriteLine($"The {userInput}  number is : {yourPhoneBook.phoneBook[userInput]}");
+                }
+                else
+                {
+                    Console.WriteLine($"That name is not found in phonebook");
+                }
+
+                Console.WriteLine("Do you whant another search? Y/n");
+                var userInput3 = Console.ReadLine();
+                if (userInput3 == "n" || userInput3 == "N") 
+                {
+                    askAgain = false;
+                    break;
+                }
+
+                askAgain = true;  
+            }
+        }
+    }
+}
